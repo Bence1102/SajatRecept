b73a2ddba5cb3a27724a038451e0493c3f28a2e2
Bence1102 <szente.bence04@gmail.com>
2025-08-20 13:42:03 +0200

    ReceptGyakorlas

---
+++ receptek.js
@@ -0,0 +1,48 @@
+export const receptLista = [
+  {
+    nev: 'Margherita pizza',
+    kep: 'kepek/pizza.jpg',
+    leiras: 'Klasszikus olasz pizza paradicsomszósszal, mozzarellával és friss bazsalikommal.',
+    hozzavalok: [
+      '1 pizzatészta',
+      '200 g paradicsomszósz',
+      '200 g mozzarella',
+      'Friss bazsalikom levelek',
+      'Olívaolaj',
+      'Só, bors ízlés szerint'
+    ],
+    kedvenc: false,
+    kategoria: 'Főétel'
+  },
+  {
+    nev: 'Tiramisu',
+    kep: 'kepek/tiramisu.jpeg',
+    leiras: 'Könnyű, krémes olasz desszert kávéval és kakaóval.',
+    hozzavalok: [
+      '250 g mascarpone',
+      '3 db tojás',
+      '3 evőkanál cukor',
+      '200 g babapiskóta',
+      '2 dl erős kávé',
+      'Kakaópor a tetejére'
+    ],
+    kedvenc: false,
+    kategoria: 'Desszert'
+  },
+  {
+    nev: 'Görög saláta',
+    kep: 'kepek/gorogsalata.jpg',
+    leiras: 'Frissítő saláta uborkával, paradicsommal, olívabogyóval és feta sajttal.',
+    hozzavalok: [
+      '2 db paradicsom',
+      '1 db kígyóuborka',
+      '1 db lilahagyma',
+      '100 g feta sajt',
+      'Fekete olívabogyó',
+      'Olívaolaj',
+      'Oregánó'
+    ],
+    kedvenc: false,
+    kategoria: 'Saláta'
+  }
+]
